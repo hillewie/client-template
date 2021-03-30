79b2b42780b7aaaa0d96a19ffa08fdb8d8e3ecf5
HILLELWIE_DAHLIN <HILLEWIE.DAHLIN@LIVE.SE>
2021-03-30 17:53:51 +0200

    Jason deuuloo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Tweets from './pages/Tweets.js';
+import Nav from './components/Nav'
+import ManageTweets from './pages/admin/ManageTweets'
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route
+} from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav/>
+
+    <Switch>
+      <Route path="/" exact component={Tweets} />
+      
+    </Switch>
+
+      FNITTER
+      <Tweets />
+
+      Admin
     </div>
+    </Router>
   );
 }
 
